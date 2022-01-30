613029124bd108e849697ef15b2df273733354e4
N0tAdm1n <n0tadm1n29@gmail.com>
2022-01-30 18:47:23 +0530

    Added event listeners to most of the buttons get registered

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstOperand;
+let secondOperand;
+let currentOperator;
+
 function add(a, b) {
   return a + b;
 }
@@ -26,3 +30,33 @@ function operate(a, b, operator) {
       return divide(a, b);
   }
 }
+// function to display to the screen
+function displayToScreen() {
+    display.textContent += this.textContent;
+}
+
+// function to clear the screen 
+function clearDisplay() {
+    display.textContent = "";
+}
+
+const digits = document.querySelectorAll(".digit");
+digits.forEach(digit => digit.addEventListener("click", displayToScreen));
+
+const display = document.querySelector("#display");
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearDisplay);
+
+// function runs when an operator is pressed
+function clickOperator() {
+    firstOperand = Number(display.textContent);
+    currentOperator = this.textContent;
+    clearDisplay();
+}
+// function to get second operand
+function getSecondOperand() {
+    secondOperand = Number(display.textContent);
+}
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => operator.addEventListener('click', clickOperator));
