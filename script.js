57f32282ab541d10e3171299db0c690ea78470a7
N0tAdm1n <n0tadm1n29@gmail.com>
2022-01-30 13:26:44 +0530

    Added operate() that calls other arithmetic functions

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
 }
